5c8d9723c3ca11e68f4c2d59f50a6cce5ece3af0
Lidia Luncian <lidia.luncian@qinsoft.ro>
2022-09-14 11:09:56 +0300

    user controller

--- module/Cardio/config/module.config.php
+++ module/Cardio/config/module.config.php
@@ -1,12 +1,25 @@
 <?php
 namespace Cardio;
 
-use Laminas\ServiceManager\Factory\InvokableFactory;
+use Laminas\Router\Http\Segment;
 
 return [
-    'controllers' => [
-        'factories' => [
-            Controller\CardioController::class => InvokableFactory::class,
+    'router' => [
+        'routes' => [
+            'user' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/user[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\UserController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
         ],
     ],
     'view_manager' => [
---
+++ module/Cardio/src/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+namespace Cardio\Controller;
+use Cardio\Model\UserTable;
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+
+class UserController extends AbstractActionController{
+
+    private $table;
+
+    public function __construct(UserTable $table){
+        $this->table = $table;
+    }
+
+    public function indexAction()
+    {
+        return new ViewModel([
+            'users' => $this->table->fetchAll(),
+        ]);
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
